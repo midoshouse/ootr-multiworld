ef95362628638012f7e1967f330b7c7db75be8ac
Fenhl <fenhl@fenhl.net>
2025-08-05 11:25:20 +0000

    pre-commit: Better error reporting

--- assets/pre-commit.ps1
+++ assets/pre-commit.ps1
@@ -1,27 +1,38 @@
 #!/usr/bin/env pwsh
 
-function ThrowOnNativeFailure {
-    if (-not $?)
-    {
-        throw 'Native Failure'
-    }
+cargo check --package=multiworld # for checking without sqlx feature
+if (-not $?)
+{
+    throw 'Native Failure'
 }
 
-cargo check --package=multiworld # for checking without sqlx feature
-ThrowOnNativeFailure
-
 cargo check --workspace
-ThrowOnNativeFailure
+if (-not $?)
+{
+    throw 'Native Failure'
+}
 
 cargo sqlx prepare --workspace --check
-ThrowOnNativeFailure
+if (-not $?)
+{
+    throw 'Native Failure'
+}
 
 wsl rustup update stable
-ThrowOnNativeFailure
+if (-not $?)
+{
+    throw 'Native Failure'
+}
 
 # copy the tree to the WSL file system to improve compile times
 wsl rsync --delete -av /mnt/c/Users/fenhl/git/github.com/midoshouse/ootr-multiworld/stage/ /home/fenhl/wslgit/github.com/midoshouse/ootr-multiworld/ --exclude .cargo/config.toml --exclude target --exclude crate/multiworld-bizhawk/OotrMultiworld/BizHawk --exclude crate/multiworld-bizhawk/OotrMultiworld/src/bin --exclude crate/multiworld-bizhawk/OotrMultiworld/src/obj --exclude crate/multiworld-bizhawk/OotrMultiworld/src/multiworld.dll
-ThrowOnNativeFailure
+if (-not $?)
+{
+    throw 'Native Failure'
+}
 
 wsl env -C /home/fenhl/wslgit/github.com/midoshouse/ootr-multiworld cargo check --workspace --exclude=multiworld-release
-ThrowOnNativeFailure
+if (-not $?)
+{
+    throw 'Native Failure'
+}
